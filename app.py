c3153f841465431e443d32545ea174711fc9292d
Paulo Nuin <nuin@genedrift.org>
2019-12-30 20:41:21 -0700

    better styling

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 import flask
 import time
-
+from flask import render_template, redirect, url_for, request
 from jinja2 import Environment
 from jinja2.loaders import FileSystemLoader
 import testing_development
 
 app = flask.Flask(__name__)
 
-@app.route('/yield')
-def index():
+@app.route('/testd')
+def testd():
     def inner():
         for x in dir(testing_development):
             item = getattr(testing_development, x)
@@ -18,18 +18,13 @@ def index():
                     time.sleep(1)
                     yield '%s<br/>\n' % item()
 
-    # def some_magic():
-    #     import a
-    #     for i in dir(a):
-    #         item = getattr(a, i)
-    #         if callable(item):
-    #             item()
-
-
-
     env = Environment(loader=FileSystemLoader('templates'))
     tmpl = env.get_template('result.html')
     return flask.Response(tmpl.generate(result=inner()))
 
+@app.route('/')
+def index():
+
+    return render_template('index.html')
 
 app.run(debug=True)
