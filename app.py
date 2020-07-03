d58f105422495f04ff65f8f19510c3a40ad4f331
Paulo Nuin <nuin@genedrift.org>
2020-07-03 14:40:09 -0600

    improved test output, added production

--- app.py
+++ app.py
@@ -3,24 +3,48 @@ import time
 from flask import render_template, redirect, url_for, request
 from jinja2 import Environment
 from jinja2.loaders import FileSystemLoader
-import testing_development
+import testing_queries
+from intermine.webservice import Service
 
 app = flask.Flask(__name__)
 
 @app.route('/testd')
 def testd():
+
+    service = Service('http://im-dev1.wormbase.org/tools/wormmine/service')
+
     def inner():
-        for x in dir(testing_development):
-            item = getattr(testing_development, x)
+        for x in dir(testing_queries):
+            item = getattr(testing_queries, x)
             print(item)
             if callable(item):
-                if not item.__name__ in ['assert_result', 'Service', 'assert_greater']:
+                if not item.__name__ in ['assert_result', 'Service', 'assert_greater', 'save_txt_file']:
                     time.sleep(1)
-                    yield '%s<br/>\n' % item()
+                    yield '%s<br/>\n' % item(service)
 
     env = Environment(loader=FileSystemLoader('templates'))
     tmpl = env.get_template('result.html')
     return flask.Response(tmpl.generate(result=inner()))
+
+
+@app.route('/testp')
+def testp():
+
+    service = Service('http://intermine.wormbase.org/tools/wormmine/service')
+
+    def inner():
+        for x in dir(testing_queries):
+            item = getattr(testing_queries, x)
+            print(item)
+            if callable(item):
+                if not item.__name__ in ['assert_result', 'Service', 'assert_greater', 'save_txt_file']:
+                    time.sleep(1)
+                    yield '%s<br/>\n' % item(service)
+
+    env = Environment(loader=FileSystemLoader('templates'))
+    tmpl = env.get_template('result.html')
+    return flask.Response(tmpl.generate(result=inner()))
+
 
 @app.route('/')
 def index():
